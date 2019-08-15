d618b9ac47b9bfed69ef973a4d1ca48feadd5d60
Jetzaj <34793620+Jetzaj@users.noreply.github.com>
2019-08-15 19:21:56 +0800

    init spring boot project

---
+++ src/main/java/com/algmachine/controller/AlgMachineApplication.java
@@ -0,0 +1,13 @@
+package com.algmachine.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgMachineApplication.class, args);
+	}
+
+}
