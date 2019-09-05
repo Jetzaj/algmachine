30c5d53dedf2f5940cc4e37fdd24b5a8d093c5b2
Jetzaj <34793620+Jetzaj@users.noreply.github.com>
2019-09-05 22:03:13 +0800

    add unit test and properties

--- src/test/java/com/algmachine/unittest/AlgMachine.java
+++ src/test/java/com/algmachine/unittest/AlgMachine.java
@@ -3,8 +3,11 @@ package com.algmachine.unittest;
 import com.algmachine.AlgMachineApplication;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = AlgMachineApplication.class)
 public class AlgMachine {
 
